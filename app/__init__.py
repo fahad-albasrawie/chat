43e18a68d1fbf01c3a15094fabc40592a39a9ea5
unknown <cabbaascadde55@gmail.com>
2024-07-10 08:07:10 +0300

    Setup changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, session
 from flask_socketio import SocketIO
 app = Flask(__name__)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet', manage_session=False)
 
 from app.public import public_views
 
