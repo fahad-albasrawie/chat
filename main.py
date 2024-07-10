cfbb768cbebd53dee0099a36571e8ae6b43a2f97
unknown <cabbaascadde55@gmail.com>
2024-07-10 08:20:58 +0300

    Setup changed2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=2025, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=2025, debug=True)
